9ec30562ba109324413ae4dae50902248b6b03b7
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-10-27 08:44:18 -0300

    Adição de audio de maneira rudimentar

--- 01 - TecladoMusical/script.js
+++ 01 - TecladoMusical/script.js
@@ -1,5 +1,7 @@
 const teclas = document.querySelectorAll(".oitava > div")
-console.log(teclas)
+const audioSource = document.getElementById("source-audio")
+
+//console.log(teclas)
 
 function desmarcar(tecla){
     tecla.classList.remove("selecionada")
@@ -8,6 +10,9 @@ function desmarcar(tecla){
 teclas.forEach(function(tecla){
     tecla.onmousedown = function(){
         tecla.classList.add("selecionada")
+        audioSource.src = `audio/${tecla.attributes[1].name}.wav`
+        console.log(tecla.attributes[1].name)
+        audioSource.play()
     }
 
 
