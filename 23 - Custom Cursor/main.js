5d7e78f366ecaf1784c24d937b5d07895e915272
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-09 22:51:45 -0300

    fim do tutorial

--- 23 - Custom Cursor/main.js
+++ 23 - Custom Cursor/main.js
@@ -1,23 +1,33 @@
 document.getElementsByTagName('body')[0].addEventListener('mousemove',
 function(n) {
-    t.style.top = n.clientX = 'px',
-    e.style.left = n.clientX = 'px',
-    e.style.top = n.clientX = 'px',
-    i.style.left = n.clientX = 'px',
-    i.style.top = n.clientX = 'px',
+    t.style.top = n.clientY + 'px',
+    t.style.left = n.clientX + 'px',
+    e.style.top = n.clientY + 'px',
+    e.style.left = n.clientX + 'px',
+    i.style.top = n.clientY + 'px',
+    i.style.left = n.clientX + 'px'
 });
 
 var t = document.getElementById('cursor'),
     e = document.getElementById('cursor2'),
     i = document.getElementById('cursor3');
 
-function n(t){
+function addHover(t){
     e.classList.add('hover'), i.classList.add('hover');
 }
 
-function s(t){
+function remoHover(t){
     e.classList.remove('hover'), i.classList.remove('hover');
 
 }
 
-8:20
+remoHover();
+
+for(var h_target = document.querySelectorAll('.hover-target'),
+        a = h_target.length -1; a>= 0; a--){
+            o(h_target[a])
+    }
+
+function o(t){
+    t.addEventListener('mouseover',addHover),t.addEventListener('mouseout',remoHover)
+}
