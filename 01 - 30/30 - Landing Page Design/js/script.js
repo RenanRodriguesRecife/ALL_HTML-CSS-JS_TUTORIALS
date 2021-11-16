a5ed0294d10be4d0f512e18e8d09df1e6c4b0aee
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-15 23:22:23 -0300

    back page animation

--- 01 - 30/30 - Landing Page Design/js/script.js
+++ 01 - 30/30 - Landing Page Design/js/script.js
@@ -5,3 +5,6 @@ t1.to('.main-heading',.6,{y: -40, opacity: 1, ease: Power0.easeIn})
 
 const t2 = new TimelineLite()
 t2.to('.images', 1,{x: -100, opacity: 1, delay: 1,ease: Power0,easeIn})
+
+const t3 = new TimelineLite()
+t3.to('.back', 1,{x: -230, ease: Power0.easeIn})
