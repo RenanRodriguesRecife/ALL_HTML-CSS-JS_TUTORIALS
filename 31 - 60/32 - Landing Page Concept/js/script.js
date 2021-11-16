2f5971de2d17c6a1afc07549d60737954cb45e2c
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-16 20:55:31 -0300

    add imagens

--- 31 - 60/32 - Landing Page Concept/js/script.js
+++ 31 - 60/32 - Landing Page Concept/js/script.js
@@ -1,13 +1,14 @@
 const displayImage = e => {
+    
     let imageId = e.id;
     let img = document.getElementById('img');
-    let doorIllusion = document.getElementByClassName('door-illusion')[0]
-    img.setAttribute('src',`../img/${imageId}.svg`)
+    let doorIllusion = document.getElementsByClassName('door-illusion')[0]
+    img.setAttribute('src',`./img/${imageId}.jpg`)
     var t1 = new TimelineLite();
 
 
     t1.fromTo(doorIllusion, 1,{boxShadow: 'inset 0em 0em 0em #000'},{boxShadow: 'inset 0em 3em 2em #000',ease:Power4.easeOut})
-      .fromTo(img, 1,{top: '240px'},{top:'80px',ease:Power4.easeOut})
+      .fromTo(img, 1,{top: '-240px'},{top:'80px',ease:Power4.easeOut})
       .fromTo(doorIllusion, .03, {boxShadow:'inset 0em 3em 2em #000'},{boxShadow: 'inset 0em 0em 0em #000', ease:Power4.easeOut})
       .fromTo(doorIllusion, .01, {overflow: 'hidden'}, {overflow: 'visible',ease:Power4.easeOut})
       .fromTo(img, 1,{scale: 1},{scale: 1.4,ease:Back.easeOut.config(4)})
