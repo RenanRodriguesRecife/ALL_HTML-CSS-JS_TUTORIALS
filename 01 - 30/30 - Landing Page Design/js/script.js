6fcee926a58c883192ed542052157069aa13510d
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-15 23:08:09 -0300

    inicio dos script de animação

---
+++ 01 - 30/30 - Landing Page Design/js/script.js
@@ -0,0 +1,4 @@
+const t1 = new TimelineLite()
+t1.to('.main-heading',.6,{y: -40, opacity: 1, ease: Power0.easeIn})
+.to('.main-para',.6,{y: -30, opacity: 1, ease:Power0.easeIn})
+.to('.btn', 1,{y: -30, opacity: 1, ease: Power0.easeIn})
