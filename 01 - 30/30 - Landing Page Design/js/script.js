46c2f61892c590dc04a369fa66fb1e471725c768
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-15 23:27:07 -0300

    add scroller animated entity

--- 01 - 30/30 - Landing Page Design/js/script.js
+++ 01 - 30/30 - Landing Page Design/js/script.js
@@ -8,3 +8,14 @@ t2.to('.images', 1,{x: -100, opacity: 1, delay: 1,ease: Power0,easeIn})
 
 const t3 = new TimelineLite()
 t3.to('.back', 1,{x: -230, ease: Power0.easeIn})
+
+const controller1 = new ScrollMagin.Controller()
+const scene1 = new ScrollMagic.Scene({
+    triggerElement: '#hero-section',
+    duration: 1500,
+    triggerHook: 0
+})
+.setTween(t3)
+.setPin('.hero-section')
+.addTo(controller1)
+
