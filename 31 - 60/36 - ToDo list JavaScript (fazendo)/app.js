1269da91fbf157e753809be726e05af8d1f42b83
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-22 00:11:26 -0300

    add to do

--- 31 - 60/36 - ToDo list JavaScript (fazendo)/app.js
+++ 31 - 60/36 - ToDo list JavaScript (fazendo)/app.js
@@ -2,4 +2,29 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-16:15
+todoButton.addEventListener('click',addTodo);
+
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+    //create todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //create li
+    const newTodo = document.createElement('li');
+    newTodo.innerHTML = 'hey';
+    newTodo.classList.add('tood-item');
+    todoDiv.appendChild(newTodo);
+    //checkmark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //checktrash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+}
