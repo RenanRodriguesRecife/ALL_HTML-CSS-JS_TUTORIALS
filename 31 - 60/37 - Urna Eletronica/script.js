ef2eb0aa52ea041951560e0f85e66aaf66aa44f4
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-23 13:29:08 -0300

    voto nulo

--- 31 - 60/37 - Urna Eletronica/script.js
+++ 31 - 60/37 - Urna Eletronica/script.js
@@ -41,7 +41,7 @@ function atualizaInterface(){
     if(candidato.length > 0){
         candidato = candidato[0];
         seuVotoPara.style.display = "block";
-        seuVotoPara.style.display = "block";
+        aviso.style.display = "block";
         descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
 
         let fotosHtml = '';
@@ -50,6 +50,10 @@ function atualizaInterface(){
             fotosHtml += `<div class="d-1-image"><img src="./img/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`
         }
         lateral.innerHTML = fotosHtml;
+    }else{
+        seuVotoPara.style.display = "block";
+        aviso.style.display = "block";
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>'
     }
 }
 
