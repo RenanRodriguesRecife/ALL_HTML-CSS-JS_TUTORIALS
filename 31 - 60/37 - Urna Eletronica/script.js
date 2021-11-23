a3f904f0e674acb7a64a166b01c3d544fbafe0ee
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-23 11:07:25 -0300

    cemeçarEtapas

--- 31 - 60/37 - Urna Eletronica/script.js
+++ 31 - 60/37 - Urna Eletronica/script.js
@@ -3,4 +3,38 @@ let cargo = document.querySelector('.d-1-2 span');
 let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
-let numeros = document.querySelector('d-1-3');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+
+
+function começarEtapa(){
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    seuVotoPara.style.display = 'none'
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function clicou(n){
+    alert("Clicou em "+n);
+}
+
+function branco(){
+    alert("Clicou em branco");
+}
+
+function corrige(){
+    alert("Clicou em corrige");
+}
+
+function confirma(){
+    alert("Clicou em confirma");
+}
+
+começarEtapa();
