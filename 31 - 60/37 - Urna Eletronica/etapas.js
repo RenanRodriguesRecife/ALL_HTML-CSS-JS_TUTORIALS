7d94697316766202c4da22b14f9c1404b4aa0c3d
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-23 10:12:09 -0300

    complete etapas

--- 31 - 60/37 - Urna Eletronica/etapas.js
+++ 31 - 60/37 - Urna Eletronica/etapas.js
@@ -5,13 +5,13 @@ let etapas = [
         candidatos: [
             {
                 numero: 11111,
-                name: 'Gordon Freeman',
+                nome: 'Gordon Freeman',
                 partido: 'Anomalous Materials',
                 fotos:[{url:'01.jpg', legenda: 'Vereador', small: false}]
             },
             {
                 numero: 22222,
-                name: 'Barney Calhoun',
+                nome: 'Barney Calhoun',
                 partido: 'Level 3 security Clearance',
                 fotos:[{url:'10.jpg', legenda: 'Vereador', small: false}]
             }
@@ -23,21 +23,39 @@ let etapas = [
         candidatos: [
             {
                 numero: 11,
-                name: 'Issac Kleiner',
+                nome: 'Issac Kleiner',
                 partido: 'Anomalous Materials',
                 vice: 'Eli Vance',
                 fotos:[
                     {url:'06.jpg', legenda: 'Prefeito', small: false},
-                    {url:'04.jpg', legenda: 'Vice-Prefeito', small: false}]
+                    {url:'04.jpg', legenda: 'Vice-Prefeito', small: true}]
             },
             {
-                numero: 11,
-                name: 'Issac Kleiner',
-                partido: 'Anomalous Materials',
-                vice: 'Eli Vance',
+                numero: 22,
+                nome: 'Colette Green',
+                partido: 'Black Mesa Science Team',
+                vice: 'Gina Cross',
                 fotos:[
-                    {url:'01.jpg', legenda: 'Prefeito', small: false},
-                    {url:'01.jpg', legenda: 'Vice-Prefeito', small: false}]
+                    {url:'02.jpg', legenda: 'Prefeito', small: false},
+                    {url:'03.jpg', legenda: 'Vice-Prefeito', small: true}]
+            },
+            {
+                numero: 33,
+                nome: 'Arne Magnusson',
+                partido: 'Anomalous Materials',
+                vice: 'Unknown guy',
+                fotos:[
+                    {url:'07.jpg', legenda: 'Prefeito', small: false},
+                    {url:'08.jpg', legenda: 'Vice-Prefeito', small: true}]
+            },
+            {
+                numero: 44,
+                nome: 'Unknown guy',
+                partido: 'Secret Science Team',
+                vice: 'Unknown guy',
+                fotos:[
+                    {url:'05.jpg', legenda: 'Prefeito', small: false},
+                    {url:'09.jpg', legenda: 'Vice-Prefeito', small: true}]
             },
         ]
     }
