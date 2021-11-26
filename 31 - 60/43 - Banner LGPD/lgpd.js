41f5e5703762d5c288a664cdeb02a6939693d166
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-26 00:34:56 -0300

    intro script

---
+++ 31 - 60/43 - Banner LGPD/lgpd.js
@@ -0,0 +1,10 @@
+/*
+    1 - verifica se existe algum registro que já aceitou o banner (cookie)
+    2 - se não encontrar aparece o banner
+*/
+
+//Conteúdo do local storage
+let lsContent = localStorage.getItem('lgpd');
+if(!liContent){
+    
+}
