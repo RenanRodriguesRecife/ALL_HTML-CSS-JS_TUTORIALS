270f90501928227b2a6a9eb48dc36d8b20c5dc70
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-25 21:28:17 -0300

    email validation

--- 31 - 60/41 - Validador de Formulario/script.js
+++ 31 - 60/41 - Validador de Formulario/script.js
@@ -41,6 +41,13 @@ let b7validator = {
                             return 'Campo tem que ter pelo menos '+rDetails[1]+' caracteres';
                         }
                     break;
+                    case 'email':
+                        if(input.value != ''){
+                            let regex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())){
+                                return 'E-mail digitado não é valido'
+                            }
+                        }
                 }
             }
         }
