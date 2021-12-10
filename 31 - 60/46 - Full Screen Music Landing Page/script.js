7d96169ff9043072b190547c758538553b11367a
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-10 13:19:14 -0300

    start script

---
+++ 31 - 60/46 - Full Screen Music Landing Page/script.js
@@ -0,0 +1,10 @@
+let musicIcon = document.getElementById('music-icon');
+let chName = document.getElementById('ch-name');
+let sgName = document.getElementById('sg-name');
+let seek = document.querySelector('.seek');
+let time = document.querySelector('.time');
+let card1 = document.querySelector('.card-1');
+let card2 = document.querySelector('.card-2');
+let card3 = document.querySelector('.card-3');
+let headingLg = document.querySelector('.heading-lg');
+
