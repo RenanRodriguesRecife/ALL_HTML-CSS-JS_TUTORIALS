7b37537ba38ffdff0cc91871eeeb1f0206db8900
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-15 12:23:31 -0300

    select links

---
+++ 31 - 60/57 - Portfolio Website Design/script.js
@@ -0,0 +1,5 @@
+//grab all links
+let logo = document.querySelector('#logo');
+let aboutLink = document.querySelector('#aboutLink');
+let skillsLink = document.querySelector('#skillsLink');
+let contactLink = document.querySelector('#contactLink');
