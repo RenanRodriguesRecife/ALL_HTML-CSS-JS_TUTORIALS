fc3122eb78b28b62857f48368264de7ae0e59731
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-15 12:26:30 -0300

    select seclector

--- 31 - 60/57 - Portfolio Website Design/script.js
+++ 31 - 60/57 - Portfolio Website Design/script.js
@@ -3,3 +3,8 @@ let logo = document.querySelector('#logo');
 let aboutLink = document.querySelector('#aboutLink');
 let skillsLink = document.querySelector('#skillsLink');
 let contactLink = document.querySelector('#contactLink');
+//grab all sections
+let main = document.querySelector('#main');
+let about = document.querySelector('#about');
+let skills = document.querySelector('#skills');
+let contact = document.querySelector('#contact');
