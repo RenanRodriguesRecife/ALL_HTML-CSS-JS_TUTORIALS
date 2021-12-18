73fbadba6d5a57b6a156f5123dd03f22f1b0cfcd
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-18 10:52:26 -0300

    minor fix on cicles id

--- 31 - 60/59 - Cicle Animated Website/script.js
+++ 31 - 60/59 - Cicle Animated Website/script.js
@@ -4,7 +4,7 @@ let furtherDetail = document.querySelector('.further-detail');
 let triangle = document.querySelector('.triangle');
 
 //loop for generation 16 circles
-for(let i=1; i<=16,i++){
+for(let i=1; i<=16;i++){
 //size of the circle
 let size = i % 2 === 0 ? `$[i + 1+ '50]` : `${i + 1}50`;
 
