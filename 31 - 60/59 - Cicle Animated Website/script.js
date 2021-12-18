1989c5380bd0d7ddb8ec67c996cd82388384197f
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-17 23:30:50 -0300

    setup variables

---
+++ 31 - 60/59 - Cicle Animated Website/script.js
@@ -0,0 +1,4 @@
+let circle = document.getElementById('circles');
+let btn = document.querySelector('.btn');
+let furtherDetail = document.querySelector('.further-detail');
+let triangle = document.querySelector('.triangle');
