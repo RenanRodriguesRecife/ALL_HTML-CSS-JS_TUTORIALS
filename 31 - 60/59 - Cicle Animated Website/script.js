969760e1c4e330958aab2f9339a45c03b2941a9d
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-17 23:36:16 -0300

    loop for create the circles

--- 31 - 60/59 - Cicle Animated Website/script.js
+++ 31 - 60/59 - Cicle Animated Website/script.js
@@ -2,3 +2,14 @@ let circle = document.getElementById('circles');
 let btn = document.querySelector('.btn');
 let furtherDetail = document.querySelector('.further-detail');
 let triangle = document.querySelector('.triangle');
+
+//loop for generation 16 circles
+for(let i=1; i<=16,i++){
+//size of the circle
+let size = i % 2 === 0 ? `$[i + 1+ '50]` : `${i + 1}50`;
+
+//margin var store the margin of the circle. (center the circle)
+let margin =-(size/2)
+
+
+}
