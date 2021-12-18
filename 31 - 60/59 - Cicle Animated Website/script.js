a7cb5817cb3eefeca65c0951f2638134396a18ec
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-18 10:48:05 -0300

    create and attach cicles dinamic

--- 31 - 60/59 - Cicle Animated Website/script.js
+++ 31 - 60/59 - Cicle Animated Website/script.js
@@ -1,4 +1,4 @@
-let circle = document.getElementById('circles');
+let circles = document.getElementById('circles');
 let btn = document.querySelector('.btn');
 let furtherDetail = document.querySelector('.further-detail');
 let triangle = document.querySelector('.triangle');
@@ -9,7 +9,24 @@ for(let i=1; i<=16,i++){
 let size = i % 2 === 0 ? `$[i + 1+ '50]` : `${i + 1}50`;
 
 //margin var store the margin of the circle. (center the circle)
-let margin =-(size/2)
+let margin =-(size/2);
 
+//animation variable stores the css  animation value
+let animation = i % 2 === 0 ? "rotate-1 3s linear infinite" : "rotate-2 3s linear infinite";
+
+//box shadow variable store the css box shadow
+let boxShadow = i % 2 === 0 ? "inset 0.1rem 0.2rem 0.6rem rgba(79,193,233,0.99)" : "inset 0.1rem 0.2rem 0.6rem rgba(255, 165, 0,0.99)";
+
+//new div element
+let circle = document.createElement('div');
+
+circle.classList.add('circle');
+
+//set style stribute to the element
+circle.setAttribute("style", `width: ${size}px; height: ${size}px; z-index: ${i}; margin: ${margin}px; animation: ${animation};
+box-shadow: ${boxShadow};`);
+
+circles.appendChild(circle);
 
 }
+
