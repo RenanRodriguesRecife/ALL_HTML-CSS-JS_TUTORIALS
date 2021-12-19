198a345a676756dcb8471057e5016e9673c31654
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-19 01:23:04 -0300

    try someting diferent

--- 01 - 30/01 - TecladoMusical/script.js
+++ 01 - 30/01 - TecladoMusical/script.js
@@ -1,8 +1,16 @@
 const teclas = document.querySelectorAll(".oitava > div")
 const audioSources = document.querySelectorAll(".source-audio")
+// const teclasObj = {}
 
+// function createAudioSource(){
+//     for(i=0;i<=teclas.length;i++){
+//         let audio = document.createElement('audio');
+//         audio.setAttribute("src",`audio/${teclas[i].attributes[1].name}.wav`);
+//         audioSources.appendChild(audio);
+//     }
+// }
 
-console.log(audioSources)
+// console.log(audioSources)
 
 function desmarcar(tecla){
     tecla.classList.remove("selecionada")
@@ -12,6 +20,7 @@ teclas.forEach(function(tecla){
     tecla.onmousedown = function(){
         tecla.classList.add("selecionada")
        
+        // console.log(tecla.attributes[1].name);
         audioSources[0].src = `audio/${tecla.attributes[1].name}.wav`
         
         audioSources[0].play()
@@ -23,6 +32,6 @@ teclas.forEach(function(tecla){
     tecla.onmouseleave = () => desmarcar(tecla)
 })
 
-
+createAudioSource();
 
 function isPlaying(audio) { return !audio.paused; }
