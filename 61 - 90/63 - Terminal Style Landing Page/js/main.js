27df85d1949b72131c24696e5f45ea4407a4dd38
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-31 10:56:11 -0300

    init winbox

--- 61 - 90/63 - Terminal Style Landing Page/js/main.js
+++ 61 - 90/63 - Terminal Style Landing Page/js/main.js
@@ -2,3 +2,7 @@ const about = document.querySelector('#about')
 const contact = document.querySelector('#contact')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
+
+about.addEventListener('click', ()=>{
+    new WinBox()
+})
