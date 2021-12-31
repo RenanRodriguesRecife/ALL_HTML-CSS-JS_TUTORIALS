51dab6bdc44d8b4bb6848215faa91b862f228852
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-12-31 10:48:33 -0300

    selectors

---
+++ 61 - 90/63 - Terminal Style Landing Page/js/main.js
@@ -0,0 +1,4 @@
+const about = document.querySelector('#about')
+const contact = document.querySelector('#contact')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
