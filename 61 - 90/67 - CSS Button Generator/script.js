eeb314d9bac0c5b660863feee7cb1fb31ad43ba8
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-05 18:28:53 -0300

    add some var, radius var and padding var

---
+++ 61 - 90/67 - CSS Button Generator/script.js
@@ -0,0 +1,16 @@
+let btnText = document.getElementById("btnText");
+let bgColor = document.getElementById("bgColor");
+let txtColor = document.getElementById("txtColor");
+let txtTransform = document.getElementById("txtTransform");
+//padding
+let tPad = document.getElementById("tPad");
+let rPad = document.getElementById("rPad");
+let bPad = document.getElementById("bPad");
+let lPad = document.getElementById("lPad");
+//radius
+let tlRad = document.getElementById("tlRad");
+let trRad = document.getElementById("trRad");
+let blRad = document.getElementById("blRad");
+let brRad = document.getElementById("brRad");
+
+let btn = document.getElementById("btn");
