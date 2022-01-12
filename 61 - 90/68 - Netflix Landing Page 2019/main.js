5c7f17dbdfc5b1dbd0c7e78398d4c130bc403a1d
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-11 23:48:46 -0300

    show content logic

--- 61 - 90/68 - Netflix Landing Page 2019/main.js
+++ 61 - 90/68 - Netflix Landing Page 2019/main.js
@@ -4,12 +4,20 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 //Select tab content item
 function selectItem(e){
     removeBorder();
+    removeShow();
     //add border to current tab
     this.classList.add('tab-border');
+
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add('show');
 }
 
 function removeBorder(){
     tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow(){
+    tabContentItems.forEach(item => item.classList.remove('show'));
 }
 
 //Listen for tab click
