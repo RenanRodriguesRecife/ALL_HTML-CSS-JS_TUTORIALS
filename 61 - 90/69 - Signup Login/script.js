49f6089c037c5aa341ada27fc418d3c687e6bf25
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-12 19:31:01 -0300

    select some vars

---
+++ 61 - 90/69 - Signup Login/script.js
@@ -0,0 +1,8 @@
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let signupForm = document.querySelector("signup .form");
+let loginForm = document.querySelector(".login .form");
+let signupMessage = document.querySelector(".signup .message");
+let loginMessage = document.querySelector(".login .message");
+let signupActivator = document.querySelector(".signup-activator");
+let loginActiator = document.querySelector(".login-activator");
