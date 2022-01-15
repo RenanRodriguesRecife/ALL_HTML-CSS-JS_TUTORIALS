5f0cfd5473881e64bd327586cc8828a757b95364
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-15 15:36:52 -0300

    base selector script

---
+++ 61 - 90/71 - Landing Page/script.js
@@ -0,0 +1,8 @@
+const collections = document.querySelector(".collections");
+const chevron = document.querySelector(".chevron");
+const signupBtn = document.querySelector(".signup");
+const signupForm = document.querySelector(".signup-form");
+const signupModalClose = document.querySelector(".signup-moda span");
+const loginBtn = document.querySelector(".login");
+const loginForm = document.querySelector(".login-form");
+const loginModalClose = document.querySelector(".login-modal span");
