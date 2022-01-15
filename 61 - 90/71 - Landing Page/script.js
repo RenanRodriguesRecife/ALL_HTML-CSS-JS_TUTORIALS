83d60091fe4e5cd9c5deb531f470f4dbab2b698d
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-15 20:23:24 -0300

    signup and login modal script

--- 61 - 90/71 - Landing Page/script.js
+++ 61 - 90/71 - Landing Page/script.js
@@ -2,7 +2,7 @@ const collections = document.querySelector(".collections");
 const chevron = document.querySelector(".chevron");
 const signupBtn = document.querySelector(".signup");
 const signupForm = document.querySelector(".signup-form");
-const signupModalClose = document.querySelector(".signup-moda span");
+const signupModalClose = document.querySelector(".signup-modal span");
 const loginBtn = document.querySelector(".login");
 const loginForm = document.querySelector(".login-form");
 const loginModalClose = document.querySelector(".login-modal span");
@@ -12,3 +12,20 @@ const showCollection = () => {
     chevron.classList.toggle('chevron-toggle');
 }
 
+chevron.addEventListener('click',showCollection);
+
+signupBtn.addEventListener('click',function(){
+    signupForm.classList.add('show-signup-modal');
+})
+
+signupModalClose.addEventListener('click',function(){
+    signupForm.classList.add('show-signup-modal');
+})
+
+signupBtn.addEventListener('click',function(){
+    loginForm.classList.add('show-login-modal');
+})
+
+signupModalClose.addEventListener('click',function(){
+    loginForm.classList.add('show-signup-modal');
+})
