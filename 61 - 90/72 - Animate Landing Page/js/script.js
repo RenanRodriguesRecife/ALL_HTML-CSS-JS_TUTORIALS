e2f9abd93d574c7d3fb7a6557c6494dc4435b852
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-16 20:56:34 -0300

    var selectors js

---
+++ 61 - 90/72 - Animate Landing Page/js/script.js
@@ -0,0 +1,10 @@
+const onLoad = () => {
+    const whiteBox = document.querySelector(".white-box");
+    const slideImg1 = document.querySelector(".slide-1 img");
+    const slidePricel = document.querySelector(".price p");
+    const itemName = document.querySelector(".item-name");
+    const rating = document.querySelector(".rating");
+    const generalName = document.querySelector(".numbers");
+    const numbers = document.querySelector(".numbers");
+    const btn = document.querySelector(".btn");
+}
