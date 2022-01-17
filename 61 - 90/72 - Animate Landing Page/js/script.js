d10cd9148a9df235e008e2ef19b0e7c75916801c
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-16 21:54:53 -0300

    time line scripts

--- 61 - 90/72 - Animate Landing Page/js/script.js
+++ 61 - 90/72 - Animate Landing Page/js/script.js
@@ -1,7 +1,7 @@
 const onLoad = () => {
     const whiteBox = document.querySelector(".white-box");
     const slideImg1 = document.querySelector(".slide-1 img");
-    const slidePricel = document.querySelector(".price p");
+    const slidePrice1 = document.querySelector(".price p");
     const itemName = document.querySelector(".item-name");
     const rating = document.querySelector(".rating");
     const generalName = document.querySelector(".numbers");
@@ -11,5 +11,12 @@ const onLoad = () => {
     let t1 = new TimelineLite();
 
     t1.fromTo(whiteBox, 1,{height: '0vh'},{height: '55vh'})
+      .fromTo(slideImg1, 1 ,{opacity: 0},{opacity: 1})
+      .fromTo(numbers, 1, {opacity: 0},{opacity: 1})
+      .fromTo(itemName, 1,{opacity: 0},{opacity: 1})
+      .fromTo(rating, 1,{opacity: 0,y:'-20px'},{opacity: 1,y: '0px',ease:Power4.easeOut},'start')
+      .fromTo(generalName, 1, {marginTop: '120px'},{marginTop: '0px',ease:Power4.easeOut},'start')
+      .fromTo(slidePrice1, 1, {opacity: 0, y: '20px'},{opacity: 1,y: '0px',ease:Power4.easeOut},'start')
+      .fromTo(btn, 1,{opacity: 0},{opacity:1},'start')
 };
 
