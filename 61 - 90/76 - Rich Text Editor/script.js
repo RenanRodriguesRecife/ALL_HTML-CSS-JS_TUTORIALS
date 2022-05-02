a1599e18005ba0c310555a6c19b2349947fbca19
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-02 15:40:02 -0300

    highlight button

--- 61 - 90/76 - Rich Text Editor/script.js
+++ 61 - 90/76 - Rich Text Editor/script.js
@@ -1,6 +1,6 @@
 let optionsButtons = document.querySelectorAll(".option-button");
 let advencedOptionButton = document.querySelectorAll(".adv-option-button");
-let fontName = document.getElementsById("fontName");
+let fontName = document.getElementById("fontName");
 let fontSizeRef = document.getElementById("fontSize");
 let writingArea = document.getElementById("text-input");
 let linkButton = document.getElementsByName("createLink");
@@ -21,3 +21,38 @@ let fontList = [
     "cursive"
 ];
 
+const initialize = () => {
+    //iniciando os botões destacados
+    highlighter(alignButtons, true);
+    highlighter(spacingButtons, true);
+    highlighter(formatButtons, false);
+    highlighter(scriptButtons, true);
+}
+
+//Destaca botões clicados
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button)=>{
+        button.addEventListener("click",()=>{
+            // needremovel significa que um unico botão deve está destacado
+            if(needsRemoval){
+                let alreadyActive = false;
+
+                //se você já clicou no botão ele permanece ativo
+                if(button.classList.contains("active")){
+                    alreadyActive = true;
+                }
+
+                //Remove o destaque dos outros botões
+                highlighterRemove(className);
+                if(!alreadyActive){
+                    //destaca botão clicado
+                    button.classList.add("active");
+                }
+            }
+            else{
+                //se outros botões pode ser destacados
+                button.classList.toggle("active");
+            }
+        })
+    })
+}
