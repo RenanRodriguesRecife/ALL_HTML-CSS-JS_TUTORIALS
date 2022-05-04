f14a760902882884bce405d75cdf6e77ca15d657
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-04 11:02:33 -0300

    elements references

---
+++ 61 - 90/77 - Pixel Art Maker/script.js
@@ -0,0 +1,10 @@
+// initial references
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementById("erase-btn");
+let paintBtn = document.getElementById("paint-btn");
+let widthValue = document.getElementById("width-value");
+let HeightValue = document.getElementById("height-value");
