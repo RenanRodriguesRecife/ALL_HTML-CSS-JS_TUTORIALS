25a6893162f5c80333949f45e4e1ad2ebab5eb25
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-21 14:31:54 -0300

    update level info

--- 61 - 90/80 - Detect Battery Status/script.js
+++ 61 - 90/80 - Detect Battery Status/script.js
@@ -15,6 +15,7 @@ window.onload = () => {
 navigator.getBattery().then((battery)=>{
     function updateAllBatteryInfo(){
         updateChargingInfo();
+        updateLevelInfo();
     }
 
     updateAllBatteryInfo();
@@ -24,14 +25,33 @@ navigator.getBattery().then((battery)=>{
         updateAllBatteryInfo();
     })
 
+    //quando o level de bateria muda
+    battery.addEventListener("levelchange",()=>{
+        updateAllBatteryInfo();
+    })
+
     function updateChargingInfo(){
         if(battery.charging){
+            chargingTimeRef.innerText = "";
             charge.classList.add("active")
             console.log("charging");
         }else{
             charge.classList.remove("active")
             console.log("not charging");
+
+            //mostra do tempo que falta
+            if(parseInt(battery.dischargingTime)){
+                let hr = parseInt(battery.dischargingTime/3600);
+                let min = parseInt(battery.dischargingTime/60 - hr * 60);
+                chargingTimeRef.innerText = `${hr}hr ${min}mins remaining`;
+            }
         }
+    }
+
+    function updateLevelInfo(){
+        let batteryLevel = `${parseInt(battery.level*100)}%`
+        charge.style.width = batteryLevel;
+        chargeLevel.textContent = batteryLevel;
     }
 });
 
