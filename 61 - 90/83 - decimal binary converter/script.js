a70a233920099397125af725e40b2099ed87780d
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-25 17:19:45 -0300

    convert dec -> bin

--- 61 - 90/83 - decimal binary converter/script.js
+++ 61 - 90/83 - decimal binary converter/script.js
@@ -2,3 +2,8 @@ let decInp = document.getElementById("dec-inp");
 let binInp = document.getElementById("bin-inp");
 let errorMsg = document.getElementById("error-msg");
 
+// decimal -> binário
+decInp.addEventListener("input",()=>{
+    let decValue = parseInt(decInp.value);
+    binInp.value = decValue.toString(2)
+})
