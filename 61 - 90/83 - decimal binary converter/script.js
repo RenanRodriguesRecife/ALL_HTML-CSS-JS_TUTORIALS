f520025c1f9e61daf321a7118c196d461f46a79f
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-25 17:31:59 -0300

    validação binário

--- 61 - 90/83 - decimal binary converter/script.js
+++ 61 - 90/83 - decimal binary converter/script.js
@@ -7,3 +7,23 @@ decInp.addEventListener("input",()=>{
     let decValue = parseInt(decInp.value);
     binInp.value = decValue.toString(2)
 })
+
+// binário -> decimal 
+binInp.addEventListeners("input",() =>{
+    let binValue = binInp.value;
+    // se o número binário é válido
+    if(binValidator(binValue)){
+
+    }else{
+
+    }
+
+    // verifica se o número binário não contem nenhum número diferente de 0 e 1
+    function binValidator(num){
+        for(let i = 0; i< num.length;i++){
+            if(num[i]!="0" && num[i] !="1"){
+                return false;
+            }
+        }
+    }
+})
